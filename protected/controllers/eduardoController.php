693c6dc67aa063136a8a73f737d03ee8fa60ea5b
grijalva <edu7art@hotmail.com>
2013-07-26 22:33:25 -0600

    probando...

---
+++ protected/controllers/eduardoController.php
@@ -0,0 +1,25 @@
+<?php
+
+class eduardoController extends Controller
+{
+	public function actionIndex()
+	{
+		// renders the view file 'protected/views/site/index.php'
+		// using the default layout 'protected/views/layouts/main.php'
+		$this->render('index');
+	}
+	public function actionError()
+	{
+		if($error=Yii::app()->errorHandler->error)
+		{
+			if(Yii::app()->request->isAjaxRequest)
+				echo $error['message'];
+			else
+				$this->render('error', $error);
+		}
+	}
+	public function actionSaludo()
+	{
+	$this->render('prueba');
+	}
+}
