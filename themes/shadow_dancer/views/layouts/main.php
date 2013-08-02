ea3184bcb444a5b55c6addeeacaea65ea3b42272
grijalva <edu7art@hotmail.com>
2013-08-01 20:26:55 -0600

    Modificando lo ultimo para trabajar con el tema

--- themes/shadow_dancer/views/layouts/main.php
+++ themes/shadow_dancer/views/layouts/main.php
@@ -31,7 +31,7 @@
 		<div class="topnav_text"><a href='#'>Home</a> | <a href='#'>My Account</a> | <a href='#'>Settings</a> | <a href='#'>Logout</a> </div>
 	</div>
 	<div id="header">
-		<div id="logo"><img src="<?php echo Yii::app()->theme->baseUrl; ?>/images/logo.png"></img><?php //echo CHtml::encode(Yii::app()->name); ?></div>
+		<div id="logo"><img src="<?php echo Yii::app()->theme->baseUrl; ?>/images/logo.png"></img><?php echo CHtml::encode(Yii::app()->name); ?></div>
 	</div><!-- header -->
     <!--
 <?php /*$this->widget('application.extensions.mbmenu.MbMenu',array(
@@ -63,12 +63,15 @@
     
 		<?php $this->widget('zii.widgets.CMenu',array(
 			'items'=>array(
-				array('label'=>'Dashboard', 'url'=>array('/site/index')),
-				array('label'=>'Graphs', 'url'=>array('/site/page', 'view'=>'graphs'),'itemOptions'=>array('class'=>'icon_chart')),
+				array('label'=>'Admin', 'url'=>array('/admin')),
+				array('label'=>'Bodega', 'url'=>array('/Bodega')),
+				array('label'=>'Pedidos', 'url'=>array('/Pedidos')),
+				
+				/**array('label'=>'Graphs', 'url'=>array('/site/page', 'view'=>'graphs'),'itemOptions'=>array('class'=>'icon_chart')),
 				array('label'=>'Form', 'url'=>array('/site/page', 'view'=>'forms')),
 				array('label'=>'Interface', 'url'=>array('/site/page', 'view'=>'interface')),				
 				array('label'=>'Buttons & Icons', 'url'=>array('/site/page', 'view'=>'buttons_and_icons')),
-				array('label'=>'Error Pages', 'url'=>array('/site/page', 'view'=>'Demo 404 page')),
+				array('label'=>'Error Pages', 'url'=>array('/site/page', 'view'=>'Demo 404 page')),**/
 			),
 		)); ?>
 	</div> <!--mainmenu -->
