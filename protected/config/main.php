8123c7ceefe15d08fa6355b3ab3878c8db8a1c0d
grijalva <edu7art@hotmail.com>
2013-08-01 19:29:19 -0600

    Revert "Actiaulizacion Simple"
    This reverts commit 14354b398adc7a3fe8fb273a542123f55dcc6b4d.

--- protected/config/main.php
+++ protected/config/main.php
@@ -7,7 +7,7 @@
 // CWebApplication properties can be configured here.
 return array(
 	'basePath'=>dirname(__FILE__).DIRECTORY_SEPARATOR.'..',
-	'name'=>'Financiera ENLASE - Sistema de Inventario',
+	'name'=>'My Web Application',
 
 	// preloading 'log' component
 	'preload'=>array('log'),
